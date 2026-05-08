15a9d1a362c5df21aa28834b25d99f7edacfe7b0
Cal <callumburnsoregan@gmail.com>
2026-05-08 22:08:57 +0200

    Update AbstractService.java

--- src/main/java/me/calrl/pluginTemplate/managers/AbstractService.java
+++ src/main/java/me/calrl/pluginTemplate/managers/AbstractService.java
@@ -1,4 +1,46 @@
 package me.calrl.pluginTemplate.managers;
 
-public abstract class AbstractService {
+import me.calrl.pluginTemplate.PluginTemplate;
+import me.calrl.pluginTemplate.utils.DebugMode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractService implements ILifecycle {
+    protected final PluginTemplate plugin;
+    private final List<ILifecycle> children = new ArrayList<>();
+
+    protected AbstractService(PluginTemplate plugin) {
+        this.plugin = plugin;
+    }
+
+    protected <T extends ILifecycle> T register(T child) {
+        children.add(child);
+        return child;
+    }
+
+    @Override
+    public void onEnable() {
+        DebugMode debug = new DebugMode(plugin);
+        for (ILifecycle conf : children) {
+            conf.onEnable();
+            debug.info(String.format("Enabling: %s", conf.getClass().descriptorString()));
+        }
+    }
+
+    @Override
+    public void onReload() {
+        DebugMode debug = new DebugMode(plugin);
+        for (ILifecycle conf : children) {
+            conf.onReload();
+            debug.info(String.format("Reloading: %s", conf.getClass().descriptorString()));
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        for (ILifecycle conf : children) {
+            conf.onDisable();
+        }
+    }
 }
